00acfee2d15407cd6340f9db563218e6ea86c92b
Priyanshu3369 <priyanshuchourasiya32198@gmail.com>
2025-11-21 16:47:38 +0530

    starting fresh

--- apps/backend/src/routes/testRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const router = Router();
-
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Backend API working successfully" });
-});
-
-export default router;
--- apps/frontend/src/store/authStore.js
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-
-export const useAuthStore = create((set) => ({
-  token: null,
-  user: null,
-  setAuth: (authData) => set(authData),
-}));
